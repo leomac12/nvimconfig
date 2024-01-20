398a4403020d7df5f34a2cb9496c1e0a900708ca
leomac12 <137363312+leomac12@users.noreply.github.com>
2024-01-19 21:04:12 -0300

    Add files via upload

---
+++ lua/options.lua
@@ -0,0 +1,18 @@
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.clipboard = 'unnamedplus'
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
+vim.o.updatetime = 300
+
+vim.o.mouse ='a'
+
+vim.o.termguicolors = true
+
+vim.o.signcolumn = 'yes'
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
