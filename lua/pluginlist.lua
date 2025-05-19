ac307fae00d7b938c28162da747a0472bd996f5d
leomac12 <137363312+leomac12@users.noreply.github.com>
2025-05-19 17:03:59 -0300

    Update pluginlist.lua

--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -11,7 +11,17 @@ return{
 	"neovim/nvim-lspconfig",
 	{"akinsho/toggleterm.nvim", version = "*", config = true},
 
-	"folke/neodev.nvim",
+	{	
+    	"folke/lazydev.nvim",
+    	ft = "lua", -- only load on lua files
+    	opts = {
+      		library = {
+        		-- See the configuration section for more details
+        		-- Load luvit types when the `vim.uv` word is found
+        		{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+     			},
+    		},
+  	},
 	
 	{
 		"hrsh7th/nvim-cmp",
