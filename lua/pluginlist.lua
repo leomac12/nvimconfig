741bd6efd051c80b538e677c3d02d6922b461acb
leomac12 <137363312+leomac12@users.noreply.github.com>
2026-04-20 22:10:53 -0300

    changed color config to all black

--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -2,6 +2,15 @@ return{
 
 	{
 		"ellisonleao/gruvbox.nvim", priority = 1000, config = function()
+			require("gruvbox").setup({
+				contrast = "hard",
+         	    palette_overrides = {
+          			dark0_hard = "#000000",
+	        },
+				overrides = {
+					SignColumn = {bg = "#000000"}
+				}
+	    })
 			vim.cmd("colorscheme gruvbox")
 		end
 	},
